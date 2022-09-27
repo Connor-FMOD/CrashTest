ae7b46305836d972d71e33371c2638c015668fbc
Connor Young <connor@fmod.com>
2022-09-28 09:50:19 +1000

    Initial Commit

---
+++ Source/TestingCrash/TestingCrash.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TestingCrash.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TestingCrash, "TestingCrash" );
