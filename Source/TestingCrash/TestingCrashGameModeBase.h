bddbbe7dd6a7c0350ea96817b76b779ff2c90c21
Connor Young <connor@fmod.com>
2022-09-28 11:11:14 +1000

    This should work

---
+++ Source/TestingCrash/TestingCrashGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TestingCrashGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTINGCRASH_API ATestingCrashGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
